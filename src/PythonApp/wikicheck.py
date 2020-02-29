c748be92ca5926afe6df1bc7306b314fe46f3ca5
Neel Jay <neeljay03@outlook.com>
2020-02-29 12:20:38 -0500

    Added wikicheck.py

---
+++ src/PythonApp/wikicheck.py
@@ -0,0 +1,15 @@
+from rake_nltk import Rake
+import wikipedia
+
+
+def getFromWiki(word):
+    try:
+        page = wikipedia.page(word)
+    except:
+        return -1
+    stuff=page.summary
+    # print(stuff)
+    r=Rake()
+    r.extract_keywords_from_text(stuff)
+    ret=r.get_ranked_phrases()
+    return ret
