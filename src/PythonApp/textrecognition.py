d79cb8dd785f7d4aba0aa790aad45db4643db4a1
Phillip Guo <philliphguo@gmail.com>
2020-02-29 09:07:52 -0500

    Refined text categorization scoring system

--- src/PythonApp/textrecognition.py
+++ src/PythonApp/textrecognition.py
@@ -6,19 +6,23 @@ import cv2
 from PIL import Image
 import Levenshtein
 import string
+from nltk.corpus import wordnet
 from nltk.corpus import stopwords
 stopwords = stopwords.words('english')
 from py_thesaurus import Thesaurus
 
 def similarityScore(strlist, keywordlist):
-    threshold = 4
+    threshold = 0.5
     score = 0
     for word in strlist:
         for keyword1 in keywordlist:
             distance = Levenshtein.distance(word, keyword1)
-            if distance<threshold:
-                score += (threshold-distance)
-
+            if distance<threshold*len(word):
+                score += len(word)*threshold-distance
+    if len(strlist) == 0:
+        return 0
+    score /= len(strlist)
+    score /= len(keywordlist)
     return score
 
 def similarityScores(str, keywordlists):
@@ -32,7 +36,6 @@ def similarityScores(str, keywordlists):
     for keywordlistindex in range(len(keywordlists)):
         keywordlist = keywordlists[keywordlistindex]
         score = similarityScore(str, keywordlist)
-        print(score)
         if score>maxscore:
             maxscore = score
             whichindex = keywordlistindex
@@ -40,17 +43,28 @@ def similarityScores(str, keywordlists):
 
 def enrich(keywords):
     newkeywords = keywords.copy()
+
+
     for keyword in keywords:
-        thesaurus = Thesaurus(keyword)
-        newkeywords.extend(thesaurus.get_synonym())
-        newkeywords.extend(thesaurus.get_antonym())
+        synonyms = [] 
+#        antonyms = [] 
+  
+        for syn in wordnet.synsets(keyword): 
+            for l in syn.lemmas(): 
+                synonyms.append(l.name()) 
+#                if l.antonyms(): 
+#                    antonyms.append(l.antonyms()[0].name())
+
+        newkeywords.extend(synonyms)
+#        newkeywords.extend(antonyms)
 
     return newkeywords
 
 
-print(Thesaurus("zero").get_synonym())
+#print(enrich(["Literature", "Style", "Essay"]))
+
 str = "Developing Efficient Algorithms Quiz Review "
-keywordslist = [["Literature", "Style", "Essay"], ["algorithms", "efficiency", "Big-O"], ["Inhibit", "Zero", "Cells", "Coronavirus"]]
+keywordslist = [["Literature", "Style", "Essay"], ["algorithms", "efficiency", "Big-O"], ["Inhibit", "Zero", "Cells", "Coronavirus", "genetics"]]
 #keywordslist = [keywords for keywords in keywordslist]
 keywordslist = [enrich(keywords) for keywords in keywordslist]
 subjects = ["English", "Computer Science", "Biology", "Unknown"]
@@ -62,20 +76,26 @@ width = 640
 height = 480
 
 #period is time between checks
-period = 300
+period = 50
 
 
 while(True):
+    scores = [0 for subject in subjects]
 
-    cv2.waitKey(period)
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    for checks in range(5):
+        cv2.waitKey(period)
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
-    text = pytesseract.image_to_string(gray)
+        text = pytesseract.image_to_string(gray)
     
-    cv2.imshow('frame', gray)
-    print(text)
-    print(subjects[similarityScores(text, keywordslist)])
+        cv2.imshow('frame', gray)
+#        print(text)
+        score = similarityScores(text, keywordslist)
+        scores[score] = scores[score] + 1
+    
+    print(subjects[scores.index(max(scores))])
+    
     if cv2.waitKey(1) and 0xFF == ord('q'):
         break
 
