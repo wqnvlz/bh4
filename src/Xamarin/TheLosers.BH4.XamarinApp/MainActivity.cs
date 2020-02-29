533f2c16aefaac2fe3a722e04b7b511129dafa1a
Yueqin Wang <39781942+wqnvlz@users.noreply.github.com>
2020-02-28 20:06:58 -0500

    Add Xamarin.Forms project

--- src/Xamarin/TheLosers.BH4.XamarinApp/MainActivity.cs
+++ src/Xamarin/TheLosers.BH4.XamarinApp/MainActivity.cs
@@ -13,7 +13,7 @@ namespace TheLosers.BH4.XamarinApp
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            this.SetContentView(Resource.Layout.activity_main);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
